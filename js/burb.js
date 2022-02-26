acc89c4c43d2af17ef302feda85b71190afe9a80
Sammy-T <cyam9019@aim.com>
2022-02-26 15:53:25 -0500

    Include headers in request options

--- js/burb.js
+++ js/burb.js
@@ -1,4 +1,6 @@
 let reqForm = document.getElementById('req-form');
+let methodSelect = document.getElementById('method');
+let headersArea = document.getElementById('headers');
 
 async function fetchUrl(event) {
     event.preventDefault();
@@ -22,7 +24,7 @@ async function fetchUrl(event) {
  * @returns {Object} An object containing fetch options
  */
 function buildRequestOpts(data) {
-    return {
+    let opts = {
         method: data.get('method'),
         mode: data.get('mode'),
         cache: data.get('cache'),
@@ -30,6 +32,20 @@ function buildRequestOpts(data) {
         redirect: data.get('redirect'),
         referrerPolicy: data.get('referrer-policy')
     };
+
+    if(data.has('headers')) {
+        opts.headers = JSON.parse(data.get('headers'));
+    }
+
+    return opts;
 }
 
-reqForm.addEventListener('submit', fetchUrl)
+// Set the default text for the header options
+headersArea.textContent = '{\n\t"Content-type": "application/json; charset=UTF-8"\n}';
+
+methodSelect.addEventListener('change', event => {
+    // Disable the headers area when using a 'get' request
+    headersArea.disabled = event.target.value === 'GET';
+});
+
+reqForm.addEventListener('submit', fetchUrl);
